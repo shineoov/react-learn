227fd5d9bc4d8ff0922e06f2eedca4e9775e5635
shineoov <shineoov88@gmail.com>
2021-09-24 22:47:53 +0900

    getDerivedStateFromProps, componentDidMount 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './App.css';
-import LifeCycleEx from './R005_LifeCycleEx'
+import LifeCycleEx from './R007_LifeCycleEx'
 
 function App() {
   return (
     <div>
         <h1>Start React 200!</h1>
         <p>HTML 적용하기!</p>
-        <LifeCycleEx></LifeCycleEx>
+        <LifeCycleEx
+            prop_value = "FromApp.js"
+        />
     </div>
   );
 }
