8259a1e4110b86711121b4a6ebbadaf912552fe8
shineoov <shineoov88@gmail.com>
2021-09-25 00:53:26 +0900

    props Boolean 사용

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import './App.css';
-import PropsDataType from './R018_PropsDataType'
+import PropsBoolean from './R019_PropsBoolean'
 
 function App() {
   return (
     <div>
         <h1>Start React 200!</h1>
         <p>HTML 적용하기!</p>
-        <PropsDataType
-            String="react"
-            Number={200}
-            Boolean={1==1}
-            Array={[0, 1, 8]}
-            Object Json={{react:"리액트", twohundred:"200"}}
-            Function={console.log("FunctionProps: function!")}
-        />
+        <PropsBoolean BooleanTrueFalse={false} />
+        <PropsBoolean BooleanTrueFalse />
     </div>
   );
 }
-
 export default App;
