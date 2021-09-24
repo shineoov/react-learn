e45f221c6ea1ef415fe6f0ebee574506ec529044
shineoov <shineoov88@gmail.com>
2021-09-24 23:19:17 +0900

    props 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import Es6 from './R009_Es6'
+import Props from './R017_Props'
 
 function App() {
   return (
     <div>
         <h1>Start React 200!</h1>
         <p>HTML 적용하기!</p>
-        <Es6/>
+        <Props props_val="THIS IS PROPS!!"/>
+        <p>^^^^^^^^^^^^^^^^^^</p>
     </div>
   );
 }
