bd5d69515d3e2d88d660158389539cd4a505f719
shineoov <shineoov88@gmail.com>
2021-09-25 01:42:27 +0900

    props  필수 값 & 기본값

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import PropsObjVal from './R020_PropsObjVal'
+import PropsDefault from './R022_PropsDefault'
 
 function App() {
   return (
     <div>
         <h1>Start React 200!</h1>
         <p>HTML 적용하기!</p>
-        <PropsObjVal Object Json={{react:"리액트!", twohundred:"200"}}/>
+        <PropsDefault ReactNumber={200}/>
     </div>
   );
 }
