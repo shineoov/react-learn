7ea6fecc064740148efa6eb11317833ea7e86ac9
shineoov <shineoov88@gmail.com>
2021-09-25 15:50:30 +0900

    shallow-equal 사용

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import ForceUpdate from './R026_ForceUpdate'
+import ShallowEqual from './R029_ShallowEqual'
 
 function App() {
   return (
     <div>
         <h1>Start React 200!</h1>
         <p>HTML 적용하기!</p>
-        <ForceUpdate/>
+        <ShallowEqual/>
     </div>
   );
 }
